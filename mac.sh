22fb6dd9531bc9d7557cc982d52aa034c9026d8e
raozhanping <raozhanping@didiglobal.com>
2025-01-13 18:44:58 +0800

    feat: 新增ohmyzsh脚本

---
+++ mac.sh
@@ -0,0 +1,7 @@
+
+# Brew
+/bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
+echo 'export PATH="/opt/homebrew/bin:$PATH"' >> \~/.bash_profile
+source \~/.bash_profile
+
+
