782ccb21f088f1ab52474c75df7b7227ca74f00e
raozhanping <raozhanping@didiglobal.com>
2026-01-16 15:24:19 +0800

    feat: mac setup

--- mac.sh
+++ mac.sh
@@ -1,7 +1,20 @@
 
-# Brew
+# brew
 /bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
 echo 'export PATH="/opt/homebrew/bin:$PATH"' >> \~/.bash_profile
 source \~/.bash_profile
 
+# ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install autojump
 
+
+# fnm Fast Node Manager
+brew install fnm
+
+
+# bun
+curl -fsSL https://bun.net.cn/install | bash # for macOS, Linux, and WSL
+
+# claude
+bun add -g @anthropic-ai/claude-code
